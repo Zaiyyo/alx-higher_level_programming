69efc8791ff5df06fc2231ddf654841e7ad43d05
Zainab <azaynab.2019@gmail.com>
2023-11-30 12:05:06 -0800

    task no. checks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def main():
-    sum = 0
-    for ii in range(1, len(argv)):
-        sum = sum + int(argv[1])
-    print(sum)
-
-
 if __name__ == "__main__":
-    main()
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
