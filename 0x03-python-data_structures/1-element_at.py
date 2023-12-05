30a5f8683540fefac1e679e4242f480728e6cbe0
Zainab <azaynab.2019@gmail.com>
2023-12-04 18:47:04 -0800

    tasks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
-
-def print_list_integer(my_list=[]):
-    """
-    prints a list of given integers
-    """
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
