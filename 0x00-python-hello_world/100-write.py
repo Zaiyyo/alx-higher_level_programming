371f19c6a8e4ebd0b4003342ad0718f5226198a5
Zainab <azaynab.2019@gmail.com>
2024-03-18 11:41:11 -0700

    task no. 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
 sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-
-
-def print_to_stderr(*a):
-    sys.stdout.write(1)
+sys.stderr.write("1")
